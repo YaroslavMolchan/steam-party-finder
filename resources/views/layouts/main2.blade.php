034653fd5239b1a4e764ab079cbe6784cdcc8538
Yaroslav Molchan <yaroslav.molchan@gmail.com>
2016-08-10 23:38:54 +0300

    teams template creating

--- resources/views/layouts/main2.blade.php
+++ resources/views/layouts/main2.blade.php
@@ -65,59 +65,82 @@
         </div>
     </section>
 
+    <div class="container youplay-user">
+        <a href="assets/images/user-photo.jpg" class="angled-img image-popup">
+            <div class="img">
+                <img src="assets/images/user-avatar.jpg" alt="">
+            </div>
+            <i class="fa fa-search-plus icon"></i>
+        </a>
+
+        <div class="user-data">
+            <h2>John Doe</h2>
+            <div class="location"><i class="fa fa-map-marker"></i> Los Angeles</div>
+            <div class="activity">
+                <div>
+                    <div class="num">69</div>
+                    <div class="title">Posts</div>
+                </div>
+                <div>
+                    <div class="num">12</div>
+                    <div class="title">Games</div>
+                </div>
+                <div>
+                    <div class="num">689</div>
+                    <div class="title">Followers</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <a class="youplay-single-match" href="match.html">
+        <div class="pull-left">
+            <div class="angled-img">
+                <div class="img">
+                    <img src="assets/images/clan-youplay.jpg" alt="Youplay">
+                </div>
+            </div>
+            <h5 class="text-center">Youplay</h5>
+        </div>
+        <div class="pull-left ml-30">
+            <div class="angled-img">
+                <div class="img">
+                    <img src="assets/images/clan-team-secret.jpg" alt="Team Secret">
+                </div>
+            </div>
+            <h5 class="text-center">Team Secret</h5>
+        </div>
+        <div class="pull-left ml-30">
+            <h3 class="youplay-match-title">The International 2015</h3>
+            <div class="date">17th February 2016, 6:00 am</div>
+        </div>
+        <div class="label youplay-match-count">10 : 7</div>
+        <div class="clearfix"></div>
+    </a>
 
     <div class="youplay-content">
-        <h2 class="container h1" id="about">Lobbies</h2>
         <section class="youplay-features container mnb-30" id="lobbies">
-            <div class="youplay-matches-list">
-                <div class="youplay-single-match">
-
+            <div class="col-sm-12">
+                <div class="feature angled-bg">
                     <div class="pull-left">
-                        <h5 class="text-center">NAVI</h5>
-                        <div class="angled-img center-block">
-                            <div class="img">
-                                <img src="/images/clan-navi.jpg" alt="NAVI">
-                            </div>
-                        </div>
-                        <div style="margin-top: 10px; margin-bottom: 10px;" class="text-center"><i class="fa fa-clock-o" aria-hidden="true"></i>1.5 <i class="fa fa-trophy" aria-hidden="true"></i>223 <i class="fa fa-dot-circle-o" aria-hidden="true"></i>312.3%</div>
+                        <h3>Empty. Create lobby first!</h3>
                     </div>
-
-                    <div class="pull-left ml-30">
-                        <div class="angled-img">
-                            <div class="img">
-                                <img src="/images/clan-fnatic.jpg" alt="fnatic">
-                            </div>
-                        </div>
-                        <h5 class="text-center">fnatic</h5>
-                    </div>
-                    <div class="pull-left ml-30">
-                        <div class="angled-img">
-                            <div class="img">
-                                <img src="/images/clan-fnatic.jpg" alt="fnatic">
-                            </div>
-                        </div>
-                        <h5 class="text-center">fnatic</h5>
-                    </div>
-                    <div class="pull-left ml-30">
-                        <div class="angled-img">
-                            <div class="img">
-                                <img src="/images/clan-fnatic.jpg" alt="fnatic">
-                            </div>
-                        </div>
-                        <h5 class="text-center">fnatic</h5>
-                    </div>
-
-
-                    <div class="pull-left ml-30">
-                        <h3 class="youplay-match-title">Red Dot Invitational</h3>
-                        <div class="date">10th February 2016, 10:30 am</div>
-                    </div>
-
                     <div class="pull-right">
-                        <a class="btn btn-md btn-white" href="#!" style="margin-top: 3rem;"><i class="fa fa-steam"></i>&nbsp;
-                            Login</a>
+                        <a class="btn btn-md active" href="#!">Create lobby</a>
                     </div>
                     <div class="clearfix"></div>
+                </div>
+            </div>
+
+            <div class="col-sm-12">
+                <div class="feature angled-bg">
+                    <a href="/asd" style="z-index: 2"><img class="avatar"
+                                        src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/95/9561d6d2da2e4805bc0bc74f88d7856950412c47_full.jpg"
+                                        alt=""></a>
+                    <h3>Dynamic Fights</h3>
+                    <div>
+                        The most dynamic fights will not make you bored
+                    </div>
                 </div>
             </div>
         </section>
